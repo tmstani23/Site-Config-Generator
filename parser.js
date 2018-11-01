2455b14544bd93913c37e6c962deab79430e338c
Tim Work Laptop <tmstani23@gmail.com>
2018-11-01 15:30:12 -0500

    Update 4
    Added function that asks user for a range of cells to parse from the excel spreadsheet. The function then parses the correct range into a object with all the headings links and labels.  Added example returned object for use in parsing the correct segments into arrays.

--- parser.js
+++ parser.js
@@ -3,32 +3,97 @@ const excelToJson = require('convert-excel-to-json');
 const readline = require('readline-sync');
 
 
-//let labelRange = readline.question("What is the first range?");
-//let linkRange = readline.question("What is the link range?");
-//let LABELS_STRING = readline.question("Enter labels");
+//const LABELS_STRING = readline.question("Enter labels");
+//console.log(LABELS_STRING);
+//const linkRange = readline.question("What is the link range? (ex:B66:C85)");
 
-const LABELS_STRING = "Products Applications Support";
+// const parsedExcel = excelToJson({
+// 	sourceFile: 'test_config.xlsx',
+// 	range: linkRange, //'B82:C86'
+// 	sheets: ['themeConfig Request']
+// });
+
+
+//const LABELS_STRING = "Products Applications Support";
 let linksArr = ["[URL.SGB_RoadSafety_DocumentLibrary_AU]","[URL.CORP_FUZEExp_Safety_Stories_AU]?key=c-Science%20of%20Road%20Safety",
 "[URL.SGB_RoadSafety_ScienceofRoadSafety_ScienceOfRoadSafetyVideoSeries_AU]","[URL.SGB_RoadSafety_ReflectiveCertifiedPartners_AU]"];
 let namesArr = ["Contact Us", "FAQ", "Where to Buy", "Warranty Registration"];
 
 let labelsArr = [];
+let parsedSegments = [];
 let idsArr = [];
 let idString = "";
 let labelText = [];
 let categoriesText = [];
-// function result = excelToJson({
-// 	sourceFile: 'test_config.xlsx',
-// 	range: `${labelRange}:${linkRange}`, //'B82:C86'
-// 	sheets: ['themeConfig Request']
-// });
+// [ 
+// 	{"themeConfig Request":
+// 		[
+// 			{
+// 				"B":"製品",
+// 				"C":"(Provide link in this cell, only if no dropdown is desired)"},
+// 			{
+// 				"B":"ネクスケア™ 製品 / ばんそうこう・テープ製品",
+// 				"C":"[CORP_FUZEExp_JP_All3MProducts]?N=5002385+8709316+8711017+8711752+3294803017&rt=r3"},
+// 			{
+// 				"B":"フツロ™ 製品 / サポーター製品",
+// 				"C":"[CORP_FUZEExp_JP_All3MProducts]?N=5002385+8709316+8711017+8711744+3294803017&rt=r3"},
+// 			{
+// 				"B":"スキンケア製品",
+// 				"C":"[CBG_PersonalHealthCare_Skincare_JP]"},
+// 			{
+// 				"B":"すべて表示",
+// 				"C":"[CORP_FUZEExp_JP_All3MProducts]?N=5002385+8710669+8711017+8721561+3294803017&rt=r3"}
+// 		]
+// 	}, 
+// 	{"themeConfig Request":
+// 		[
+// 			{
+// 				"B":"ソリューション"},
+// 			{
+// 				"B":"おむつかぶれ対策",
+// 				"C":"[CBG_PersonalHealthCare_Solutions_DiaperRash_JP]"},
+// 			{
+// 				"B":"ハンドケア",
+// 				"C":"[CBG_PersonalHealthCare_Solutions_HandCare_JP]"},
+// 			{
+// 				"B":"すべて表示",
+// 				"C":"[CBG_PersonalHealthCare_Solutions_JP]"}
+// 		]
+// 	},
+// 	{"themeConfig Request":
+// 		[
+// 			{"B":"サポート"
+// 			},
+// 			{"B":"お問い合わせ",	
+// 			"C":"CBG_PersonalHealthCare_Support_ContactUs_JP"
+// 			}
+// 		]
+// 	}
+// ]
+
+
+
+function createSegments() {
+	parsedSegments = [];
+	const numLabels = readline.question("How many labels are there?");
+	for(let i=0; i<numLabels; i++) {
+		let labelRange = readline.question("What is the range of the label and its links? (ex:B66:C85)");
+		let segment = excelToJson({
+			sourceFile: 'test_config.xlsx',
+			range: labelRange, //'B82:C86'
+			sheets: ['themeConfig Request']
+		});
+		parsedSegments.push(segment)
+	}
+	console.log(JSON.stringify(parsedSegments));
+}
 
 //function to generate ssn id string
 function genIdString (labelsArr) {
 	idString = `psn.manual.ids = ${labelsArr.map(
 		(val) => val.substr(0,4)
 	).join(", ").toLowerCase()}`;
-	//console.log(idString);
+	console.log(idString);
 	return idString;
 }
 //save labels to an array
@@ -44,7 +109,7 @@ function storeIds(labelsArr) {
 	idsArr = labelsArr.map((val) =>
 		val.substr(0,4).toLowerCase()
 	);
-	//console.log(idsArr);
+	console.log(idsArr);
 	return idsArr;
 }
 
@@ -80,16 +145,12 @@ function genSsnLinks(labelsArr, linksArr) {
 
 
 
-//console.log(getRange("B82", "C86"));
-//console.log(labelRange, linkRange);
-//console.log(JSON.stringify(result, null, 4));
 
-//console.log(genId("product", 2));
-//console.log(genIdString("product", ))
-//storeLabels(LABELS_STRING);
-//genIdString(storeLabels(LABELS_STRING))
-storeLabels(LABELS_STRING);
-storeIds(storeLabels(LABELS_STRING));
-genIdString(labelsArr)
-//genSsnCategory(labelsArr, idsArr);
-genSsnLinks(labelsArr, linksArr);
+//console.log(parsedExcel);
+createSegments()
+// storeLabels(LABELS_STRING);
+//storeIds(storeLabels(LABELS_STRING));
+
+//genIdString(labelsArr)
+// genSsnCategory(labelsArr, idsArr);
+// genSsnLinks(labelsArr, linksArr);
