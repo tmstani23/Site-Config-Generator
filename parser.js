ae036113fd62e2292d85a72c3df44d5567fa605a
tmstani23 <tmstani23@gmail.com>
2018-11-04 12:47:26 -0600

    Update 7
    Added chalk for colored console returns.  Added function that asks if language is english and if not adjusts id string and ssn text name and url strings to be "item" + index instead of ssn header labels.  Added regex to format the final ssn text array into one long string separating each element with a new line without quotes.

--- parser.js
+++ parser.js
@@ -1,14 +1,16 @@
 'use strict';
 const excelToJson = require('convert-excel-to-json');
 const readline = require('readline-sync');
+const chalk = require('chalk');
 
 
 
 //const LABELS_STRING = "Products Applications Support";
 let linksArr = [];
 let namesArr = [];
-
+let isEnglish = true;
 let labelsArr = [];
+let itemLabels = [];
 //let parsedSegments = [];
 let idsArr = [];
 let idString = "";
@@ -61,10 +63,21 @@ let parsedSegments = [
 	}
 ];
 
+// B46:C50
+// B51:C54
+// B55:C56
 
+function setLanguage (ssnLanguage) {
+	if(ssnLanguage === "no") {
+		isEnglish = false;
+	}
+}
 
 function createSegments() {
 	parsedSegments = [];
+	const ssnLanguage = readline.question("Is the SSN in English? (yes/no)");
+	//set language if not english:
+	setLanguage(ssnLanguage);
 	const numLabels = readline.question("How many labels are there?");
 	for(let i=0; i<numLabels; i++) {
 		let labelRange = readline.question("What is the range of the label and its links? (ex:B66:C85)");
@@ -81,7 +94,7 @@ function createSegments() {
 
 function parseSsnObject(parsedSegments) {
 	//iterate through all the objects in the parsedSegments array
-	parsedSegments.forEach(element => {
+	parsedSegments.forEach((element, index) => {
 		let valuesObj = (Object.values(element));
 		
 		for (var key in valuesObj) {
@@ -89,15 +102,25 @@ function parseSsnObject(parsedSegments) {
 				let labelObj = valuesObj[key];
 				//console.log(labelObj);
 				//console.log(labelObj[0]);
+				 
+				
 				let label = labelObj[0].B;
+				let item = "";
+				
 				
 				let labelText = `psn.manual.${label.substr(0,4).toLowerCase()}.name = ${label}`;
 				let labelTextUrl = `psn.manual.${label.substr(0,4).toLowerCase()}.url = #`;
 				
 				
-				
+				if(isEnglish === false) {
+					item = `item${index}`;
+					labelText = `psn.manual.item${index}.name = ${label}`;
+					labelTextUrl = `psn.manual.item${index}.url = #`;
+					itemLabels.push(item);
+				}
 				//console.log(label);
 				labelsArr.push(label);
+				
 				categoriesText.push(labelText, labelTextUrl)
 
 				for (let i = 0; i<labelObj.length; i++) {
@@ -105,26 +128,38 @@ function parseSsnObject(parsedSegments) {
 					if (!i==0) {
 						
 						let linkName = labelObj[i].B;
-						let linkUrl = testUrlBrackets(labelObj[i].C);
+						let linkUrl = formatUrl(labelObj[i].C);
 						
-						genSsnText(label, linkName, linkUrl, i);
+						genSsnText(label, linkName, linkUrl, i, item);
 					}
 				}
 			}
 		}
 		
 	});
-	genIdString(labelsArr)
-	console.log(categoriesText);
+	genIdString(labelsArr, itemLabels)
+	let parsedSsn = categoriesText.join("\n");
+	console.log(chalk.magenta(parsedSsn));
 	//console.log(labelsArr);
 	//console.log(namesArr);
 	//console.log(linksArr);
+	return parsedSsn;
 	
 }
-function genSsnText(label, linkName, linkUrl, i) {
+function genSsnText(label, linkName, linkUrl, i, item) {
+	let nameText = "";
+	let urlText = "";
 	
-	let nameText = `psn.manual.${label.substr(0,4).toLowerCase()}.name.${i} = ${linkName}`;
-	let urlText = `psn.manual.${label.substr(0,4).toLowerCase()}.url.${i} = ${linkUrl}`;
+	if (isEnglish === true) {
+		nameText = `psn.manual.${label.substr(0,4).toLowerCase()}.name.${i} = ${linkName}`;
+		urlText = `psn.manual.${label.substr(0,4).toLowerCase()}.url.${i} = ${linkUrl}`;
+	}
+	else if (isEnglish == false) {
+		
+		nameText = `psn.manual.${item}.name.${i} = ${linkName}`;
+		urlText = `psn.manual.${item}.url.${i} = ${linkUrl}`;
+		
+	}
 
 	categoriesText.push(nameText, urlText);
 	
@@ -132,16 +167,23 @@ function genSsnText(label, linkName, linkUrl, i) {
 }
 
 //function to generate ssn id string
-function genIdString (labelsArr) {
-	idString = `psn.manual.ids = ${labelsArr.map(
-		(val) => val.substr(0,4)
-	).join(", ").toLowerCase()}`;
+function genIdString (labelsArr, itemLabels) {
+	if(isEnglish === true) {
+		idString = `psn.manual.ids = ${labelsArr.map(
+			(val) => val.substr(0,4)
+		).join(", ").toLowerCase()}`;
+	}
+	else if (isEnglish === false) {
+		idString = `psn.manual.ids = ${itemLabels.map(
+			(val) => val)}`;
+	}
+	
 	//console.log(idString);
 	categoriesText.unshift(idString);
 	return idString;
 }
-function testUrlBrackets(url) {
-	let newUrl = "";
+function formatUrl(url) {
+	
 	if (url[0] !== "[") {
 		url = `[${url}`;
 	}
@@ -157,10 +199,11 @@ function testUrlBrackets(url) {
 		
 		url = url.slice(0, 1) + "URL." + url.slice(1);
 	}
-	console.log(url);
+	
+	//console.log(url);
 	return url;
 }
-//testUrlBrackets("[CORP_FUZEExp_JP_All3MProducts?N=5002385+8710669+8711017+8721561+3294803017&rt=r3")
+//formatUrl("[CORP_FUZEExp_JP_All3MProducts?N=5002385+8710669+8711017+8721561+3294803017&rt=r3")
 
 
 createSegments()
